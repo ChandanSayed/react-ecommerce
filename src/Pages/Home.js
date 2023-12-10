1a5f8023d5ce4b6ecaf69289a56cebeb67bae50e
chandansayed <hangzhou635@gmail.com>
2023-12-10 08:39:07 +0600

    Theme colors and media queries added with styled-components

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,15 @@
+import styled from 'styled-components';
+
 const Home = () => {
-  return <div>Home!</div>;
+  return (
+    <Wrapper>
+      <h2>Home</h2>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  background-color: ${({ theme }) => theme.colors.bg};
+`;
 
 export default Home;
