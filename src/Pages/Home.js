75ec95e630a664133bec92c5ab0359d38099fdae
chandansayed <hangzhou635@gmail.com>
2023-12-26 18:03:24 +0600

    Services and trusted section added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,13 @@
 import HeroSection from '../Components/HeroSection';
+import Services from '../Components/Services';
+import Trusted from '../Components/Trusted';
 
 const Home = () => {
   return (
     <>
       <HeroSection name={'ES Store'} des={'Discover a curated collection of premium products at our online store. From trendy gadgets to timeless classics, find quality, style, and convenience in every purchase. Elevate your shopping experience today!'} />
+      <Services />
+      <Trusted />
     </>
   );
 };
