2bda2d2358367b319c295f3819ee88da3b6a825d
chandansayed <hangzhou635@gmail.com>
2024-01-06 23:45:17 +0600

    Unique colors code extracted

--- src/Components/FilterList.js
+++ src/Components/FilterList.js
@@ -14,12 +14,18 @@ const FilterList = () => {
       return curElem[attr];
     });
 
+    if (attr === 'colors') {
+      return (newVal = ['All', ...new Set([].concat(...newVal))]);
+    }
+
     return (newVal = ['all', ...new Set(newVal)]);
   };
 
   // we need to have the individual data of each in an array format
   const categoryData = getUniqueData(all_products, 'category');
   const companyData = getUniqueData(all_products, 'company');
+  const colorsData = getUniqueData(all_products, 'colors');
+  console.log(colorsData);
 
   return (
     <Wrapper>
